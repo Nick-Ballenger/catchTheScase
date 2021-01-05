bfaff9b82d7b26d2aa7fb81f90b9c87c7de537ce
Nick-Ballenger <nickster214@gmail.com>
2021-01-05 11:28:01 -0700

    starter

---
+++ catchthescrase/src/App.jsx
@@ -0,0 +1,27 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div className="header_image">
+
+        <div className="jiggle">
+       <img src="./hypa.gif" alt=""/>
+
+        </div>
+        <div className="joggle">
+          <img src="./milk.gif" alt=""/>
+        </div>
+       
+        </div>
+        
+        
+   
+      </header>
+    </div>
+  );
+}
+
+export default App;
